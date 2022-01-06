781ef93177e9fb68081c641b26eb16027b8c1745
angeldesweb <angeldesweb@gmail.com>
2022-01-05 19:53:03 -0800

    feat(Error): Error Handler
    Añadido manejador básico de errores y algunos códigos previsualizados para la aplicación
    
    Hecho por angeldesweb

---
+++ src/errors/codes.js
@@ -0,0 +1,11 @@
+export const codes = {
+	c10001: `Este método solo admite datos de tipo "string" con representación numérica, 
+    para mayor información sobre este error, visita la documentación.
+    Código 10001.`,
+	c10002: `La cadena introducida no representa una expresión numérica. Código 10002`,
+	c10003: `Este método solo admite datos de tipo "number", para mayor información sobre este
+    error, visita la documentación.
+    Código 10003
+    `,
+	c0: 'Error deconocido',
+};
