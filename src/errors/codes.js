3373f58c8f503fddbb8d2151059e374754f00bd8
angeldesweb <angeldesweb@gmail.com>
2022-01-06 21:02:47 -0800

    Added handlers and converter documentation

--- src/errors/codes.js
+++ src/errors/codes.js
@@ -1,11 +1,9 @@
 export const codes = {
-	c10001: `Este método solo admite datos de tipo "string" con representación numérica, 
-    para mayor información sobre este error, visita la documentación.
+	c10001: `Este método solo admite datos de tipo "string" con representación numérica.
     Código 10001.`,
 	c10002: `La cadena introducida no representa una expresión numérica. Código 10002`,
-	c10003: `Este método solo admite datos de tipo "number", para mayor información sobre este
-    error, visita la documentación.
-    Código 10003
-    `,
+	c10003: `Este método solo admite datos de tipo "number". Código 10003`,
+	c10004:
+		'La expresión no es admitida como base en string. Válidas: ["bin","oct","dec","hex"]',
 	c0: 'Error deconocido',
 };
